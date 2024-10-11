3da63fda33abfd868709ed23d7b682df4bea30a8
Giovanni <5774411+giovannicoppola@users.noreply.github.com>
2024-10-11 07:08:05 -0400

    v0.1

--- README.md
+++ README.md
@@ -1,5 +1,80 @@
-# alfred-keyknight
- conquer your keyboard
+# alfred-keyknight 
+conquer your keyboard 👇️
 
-# Acknowledgements
-https://www.flaticon.com/free-icon/wasd_8982293
+An Alfred Worfklow to review the layout of your keyboard and corresponding finger positions.
+
+
+<a href="https://github.com/giovannicoppola/alfred-keyknight/releases/latest/">
+<img alt="Downloads"
+src="https://img.shields.io/github/downloads/giovannicoppola/alfred-keyknight/total?color=purple&label=Downloads"><br/>
+</a>
+
+![](keyknight.png)
+
+
+<!-- MarkdownTOC autolink="true" bracket="round" depth="3" autoanchor="true" -->
+
+- [Motivation](#motivation)
+- [Setting up](#setting-up)
+- [Basic Usage](#usage)
+- [Known Issues](#known-issues)
+- [Acknowledgments](#acknowledgments)
+- [Changelog](#changelog)
+- [Feedback](#feedback)
+
+<!-- /MarkdownTOC -->
+
+
+
+<h1 id="motivation">Motivation ✅</h1>
+
+- Quickly check the layout and finger positions of QWERTY and Dvorak keyboard to improve your touchtyping or learn a new keyboard layout.
+
+
+<h1 id="setting-up">Setting up ⚙️</h1>
+
+- Alfred 5 with Powerpack license
+- Python3 (howto [here](https://www.freecodecamp.org/news/python-version-on-mac-update/))
+
+
+
+## Default settings 
+- In Alfred, open the 'Configure Workflow' menu in `alfred-keyknight` preferences
+	- set the keyword or hotkey to open in map mode (all keys labeled, default: `qwe`)
+	- set the keyword or hotkey to open in learning mode (no keys labeled, default: `qww`)
+	- set keyboard layout (`QWERTY` and `Dvorak` currently supported)
+	- choose the set of symbols to indicate finger positions (default: 1️⃣2️⃣3️⃣)
+	- set overall spacing
+	- set left padding
+	
+
+<h1 id="usage">Basic Usage 📖</h1>
+
+- open in map or learning mode, then press keys to see their position on the keyboard and corresponding finger
+- QuickLook (⌘-Y or ⇧) will show a diagram of the keyboard layout.
+
+
+
+<h1 id="known-issues">Limitations & known issues ⚠️</h1>
+
+- May not show spacing correctly in some Alfred themes
+
+
+
+<h1 id="acknowledgments">Acknowledgments 😀</h1>
+
+- Thanks to the [Alfred forum](https://www.alfredforum.com) community!
+- Patrick Sy ([@zeitlings](https://github.com/zeitlings) for ispiration with [Calendar++](https://github.com/zeitlings/alfred-calendar))
+- Icon from [Flaticon](https://www.flaticon.com/free-icon/wasd_8982293)
+- keyboard diagrams from https://onlinetyping.org/
+
+
+
+<h1 id="changelog">Changelog 🧰</h1>
+
+- 10-11-2024: version 0.1
+
+
+<h1 id="feedback">Feedback 🧐</h1>
+
+Feedback welcome! If you notice a bug, or have ideas for new features, please feel free to get in touch either here, or on the [Alfred](https://www.alfredforum.com) forum. 
--- source/keyknight.py
+++ source/keyknight.py
@@ -9,7 +9,8 @@ SPACING = int(os.path.expanduser(os.getenv('SPACING', '')))
 LEFT_PAD = int(os.path.expanduser(os.getenv('LEFT_PAD', '')))
 WF_PATH = os.getenv('alfred_preferences')
 WF_BUNDLE = os.getenv('alfred_workflow_bundleid')
-SYMBOL_SET = "keycap"
+SYMBOL_SET = os.path.expanduser(os.getenv('SYM_SERIES', ''))
+LAYOUT = os.path.expanduser(os.getenv('LAYOUT', ''))
 
 
 
@@ -20,17 +21,6 @@ def log(s, *args):
 
 
 
-def format_string(myString, target_char=None):
-    # Define the string 'qwerty'
-    chars = list(myString)
-    
-    # If the user has provided a target character, add '|' around it
-    if target_char in chars:
-        chars = [f"|{char}|" if char == target_char else char for char in chars]
-    
-    # Join the characters with two spaces
-    return '  '.join(chars)
-
 
 def addItem (myString, myResults):
 
@@ -38,7 +28,7 @@ def addItem (myString, myResults):
             "title": myString.upper(),
             'subtitle': "",
             'valid': True,
-            "quicklookurl": f'{WF_PATH}/workflows/{WF_BUNDLE}/touch-typing-keyboard.png',
+            "quicklookurl": f'{WF_PATH}/workflows/{WF_BUNDLE}/icons/{LAYOUT}.png',
             "mods": {
                     "alt": {
                         "valid": False,
@@ -68,80 +58,51 @@ def addItem (myString, myResults):
             }) 
     return myResults
 
-def defineDict ():
-    keysDict = {
-        "row0": {"`": 5,
-                 "1": 5, "2": 4, "3": 3, "4": 2, "5": 2, "6": 2, "7": 2, "8": 3, "9": 4, "0": 5, "-": 5, "=": 5},
-        "row1": {"q": 5, "w": 4, "e": 3, "r": 2, "t": 2, "y": 2, "u": 2, "i": 3, "o": 4, "p": 5, "[": 5, "]": 5},
-        "row2": {"a": 5, "s": 4, "d": 3, "f": 2, "g": 2, "h": 2, "j": 2, "k": 3, "l": 4, ";": 5, "'": 5},
-        "row3": {"z": 5, "x": 4, "c": 3, "v": 2, "b": 2, "n": 2, "m": 2, ",": 3, ".": 4, "/": 5}
-    }
+def chooseKeyboard (LAYOUT):
+    if LAYOUT == "qwerty":
+        keybLayout = {
+            "layout" : {
+                "row0": {"`": 5,
+                        "1": 5, "2": 4, "3": 3, "4": 2, "5": 2, "6": 2, "7": 2, "8": 3, "9": 4, "0": 5, "-": 5, "=": 5},
+                "row1": {"q": 5, "w": 4, "e": 3, "r": 2, "t": 2, "y": 2, "u": 2, "i": 3, "o": 4, "p": 5, "[": 5, "]": 5},
+                "row2": {"a": 5, "s": 4, "d": 3, "f": 2, "g": 2, "h": 2, "j": 2, "k": 3, "l": 4, ";": 5, "'": 5},
+                "row3": {"z": 5, "x": 4, "c": 3, "v": 2, "b": 2, "n": 2, "m": 2, ",": 3, ".": 4, "/": 5}},
+            "lefthand": "`12345qwertasdfgzxcvb",
+            "righthand": "567890-=yuioph"
+        }
+    elif LAYOUT == "dvorak":
+        keybLayout = {
+            "layout": {
+                'row0': {"1":5,"2":4,"3":3,"4":2,"5":2,"6":2,"7":2,"8":3,"9":4,"0":5,"[":5,"]":5},
+                'row1': {"'":5,",":4,".":3,"p":2,"y":2,"f":2,"g":2,"c":3,"r":4,"l":5,"/":5,"=":5},
+                'row2': {"a":5,"o":4,"e":3,"u":2,"i":2,"d":2,"h":2,"t":3,"n":4,"s":5,"-":5},
+                'row4': {";":5,"q":4,"j":3,"k":2,"x":2,"b":2,"m":2,"w":3,"v":4,"z":5}},
+            "lefthand": "12345',.pyaoeui;qjkx",
+            "righthand": "67890[]fgcrl/=dhtns-vzbmw"
+        }
 
-    dvorakDict = {
-        'row0': {"1":5,"2":4,"3":3,"4":2,"5":2,"6":2,"7":2,"8":3,"9":4,"0":5,"'":5,",":5,".":5},
-        'row1': {"p":2,"y":2,"f":2,"g":2,"c":3,"r":4,"l":5},
-        'row2': {"/":2,"=":3,"a":4,"o":3,"e":2,"u":2,"i":2,"d":2,"h":3,"t":4,"n":5,"s":5,"-":5},
-        'row4': {";":5,"q":5,"j":5,"k":5,"x":5,"b":5,"m":5,"w":5,"v":5,"z":5}
-    }
+    return keybLayout
 
-
-    return keysDict
-
-
-def format_keycap_for_character(keysDict, character):
-    result = []
-    
-   
-    for row, key_values in keysDict.items():
-        if character in key_values:
-            num = key_values[character]
-            emoji = keycap_emoji[num]
-            result.append(f"{emoji}{character}")  # Add emoji next to character
-        else:
-            # Add the key without the emoji
-            result.append(character)
-    
-    return '  '.join(result)
-
-# def format_rows(keysDict):
-#     keycap_emoji = {
-#     1: "1️⃣", 2: "2️⃣", 3: "③", 4: "4️⃣", 5: "5️⃣", 6: "⑤"
-#     #1: "1️⃣", 2: "2️⃣", 3: "3️⃣", 4: "4️⃣", 5: "5️⃣", 6: "6️⃣"
-#     }
-    
-#     formatted_output = {}
-    
-#     for row, key_values in keysDict.items():
-#         formatted_output[row] = '  '.join(f"{keycap_emoji[num]}{key}{keycap_emoji[num]}" for key, num in key_values.items())
-#         # Add the emoji only before the character
-#         formatted_output[row] = '    '.join(f"{keycap_emoji[num]}{key}" for key, num in key_values.items())
-#         # Create the left and right hand portions
-#         left_hand = '  '.join(f"{keycap_emoji[num]}{key}" for key, num in key_values.items() if key in "`qwertasdfgzxcvb12345")
-#         right_hand = '  '.join(f"{keycap_emoji[num]}{key}" for key, num in key_values.items() if key in "yuiophjklnm67890-=[];'./")
-        
-#         # Add extra space between the two hands
-#         formatted_output[row] = left_hand + "              " + right_hand  # Extra spaces between the hands
-    
-    
-#     return formatted_output
 
 def chooseEmoj (EMOJI_SET):
-    keycap_emoji = {
-    1: "1️⃣", 2: "2️⃣", 3: "3️⃣", 4: "4️⃣", 5: "5️⃣"
-    }
-    keycap_circled = {
-    1: "️⓵", 2: "⓶", 3: "③", 4: "️⓸", 5: "️⑤"
-    }
-     
-    keycap_full = {
-    1: "️⓵", 2: "⓶", 3: "③", 4: "️⓸", 5: "️⑤"
-    }
+    if EMOJI_SET == "keycaps":
+        keycap_emoji = {
+        1: "1️⃣", 2: "2️⃣", 3: "3️⃣", 4: "4️⃣", 5: "5️⃣"
+        }
+    elif EMOJI_SET == "circled":
+        keycap_emoji = {
+        1: "️⓵", 2: "⓶", 3: "③", 4: "️⓸", 5: "️⑤"
+        }
+    elif EMOJI_SET == "full":
+        keycap_emoji = {
+        1: "️❶", 2: "❷", 3: "❸", 4: "️❹", 5: "️❺"
+        }
     return keycap_emoji
-
+    
 def format_rows_with_emoji(keysDict, myMode, keycap_emoji, character=None):
     formatted_output = {}
  
-    for row, key_values in keysDict.items():
+    for row, key_values in keysDict['layout'].items():
         # Create the left and right hand portions
         left_hand = []
         right_hand = []
@@ -149,15 +110,17 @@ def format_rows_with_emoji(keysDict, myMode, keycap_emoji, character=None):
             extraSpace = (' '* SPACING*2)
         else:
             extraSpace = ''
+        log(f"row: {row}")
+        log(f"key_values: {key_values}")
         if character or myMode == 'hidden':
             
             for key, num in key_values.items():
-                if key in list(character):  # Check if the key matches the user input
+                if character and key in list(character):  # Check if the key matches the user input
                     formatted_key = f"{key}{keycap_emoji[num]}"  # Add emoji next to the character
                 else:
                     formatted_key = f"{key}"  # Just add the key normally
                 # Separate keys into left and right hands
-                if key in "`qwertasdfgzxcvb12345":
+                if key in keysDict['lefthand']:
                     left_hand.append(formatted_key)
                 else:
                     right_hand.append(formatted_key)
@@ -165,7 +128,7 @@ def format_rows_with_emoji(keysDict, myMode, keycap_emoji, character=None):
             for key, num in key_values.items():
                 formatted_key = f"{key}{keycap_emoji[num]}"  # Add emoji next to the character
                   # Separate keys into left and right hands
-                if key in "`qwertasdfgzxcvb12345":
+                if key in keysDict['lefthand']:
                     left_hand.append(formatted_key)
                 else:
                     right_hand.append(formatted_key)
@@ -178,14 +141,9 @@ def format_rows_with_emoji(keysDict, myMode, keycap_emoji, character=None):
 def main():
     result = {"items": []}
 
-    keysDict = defineDict()
+    keysDict = chooseKeyboard(LAYOUT)
     keycap_emoji = chooseEmoj(SYMBOL_SET)
-    # Get the formatted rows
-    # if MYMODE == "map":
-    #     formatted_rows = format_rows(keysDict)
-    # elif MYMODE == "hidden":
-
-        # formatted_rows = format_rows(keysDict)
+    
     formatted_rows = format_rows_with_emoji(keysDict, MYMODE, keycap_emoji, MY_INPUT)
 
     # Print the output for each row
